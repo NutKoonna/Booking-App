1abe4c25444a848c017d698840651869210a8b9a
Nut Koonna <nutquasar@gmail.com>
2023-05-28 18:59:55 +0700

    login and register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import IndexPage from './pages/IndexPage.jsx';
+import LoginPage from './pages/LoginPage';
+import Layout from './Layout';
+import RegisterPage from './pages/RegisterPage';
+import axios from 'axios';
+/* import { UserContextProvider } from './UserContext'; */
+
+axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.withCredentials = true;
 
 function App() {
- 
   return (
-    <h1 className="bg-red-500">
-    test
-   </h1>
+
+      <Routes>
+        <Route path = "/" element = {<Layout />}>
+          <Route index element = {<IndexPage/>}/>
+          <Route path='/login' element = {<LoginPage />}/>
+          <Route path='/register' element = {<RegisterPage />}/>
+        </Route>
+      </Routes>
+    
   )
 }
 

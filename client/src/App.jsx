ee1e147bdb63b1e2a533ec0e143a27dc55a3444c
Nut Koonna <nutquasar@gmail.com>
2023-06-04 02:55:22 +0700

    logout and places

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,9 +18,10 @@ function App() {
       <Routes>
         <Route path = "/" element = {<Layout />}>
           <Route index element = {<IndexPage/>}/>
-          <Route path='/login' element = {<LoginPage />}/>
-          <Route path='/register' element = {<RegisterPage />}/>
-          <Route path='/account' element = {<AccountPage />}/>
+          <Route path='/login' element = {<LoginPage/>}/>
+          <Route path='/register' element = {<RegisterPage/>}/>
+          <Route path='/account/:subpage?' element = {<AccountPage/>}/>
+          <Route path='/account/:subpage/:action' element = {<AccountPage/>}/>
         </Route>
       </Routes>
    </UserContextProvider>

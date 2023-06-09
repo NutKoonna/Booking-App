61946f1e947e88e21796252b92ec13117ad0339f
Nut Koonna <nutquasar@gmail.com>
2023-06-09 20:28:52 +0700

    change layout size

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,10 +1,10 @@
-import Header from "./Header";
+import Header from "./Header.jsx";
 import {Outlet} from "react-router-dom";
 
 export default function Layout() {
   return (
     
-    <div className="py-4 px-8 flex flex-col min-h-screen max-w-4xl mx-auto">
+    <div className="py-4 px-8 flex flex-col min-h-screen max-w-8xl mx-auto">
       <Header />
       <Outlet />
     </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#ff385c',
+        primary: '#F5385D',
       },
     },
   },
